db43ee1a9167662216366e67aa9c573bc8f3455e
mechztcc <dojathan@gmail.com>
2021-11-04 20:41:02 -0300

    aula 52

---
+++ lib/resposta.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+  final String texto;
+  final funcao;
+
+  Resposta(this.texto, this.funcao);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      width: double.infinity,
+      child: ElevatedButton(onPressed: funcao, child: Text(texto)),
+    );
+  }
+}
